8f2908123136359139e6edcbf1d801e9cc599423
yulin <2441518624@qq.com>
2019-06-27 11:26:16 +0800

    本地测试环境搭建完毕

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,35 @@
 <template>
-  <div >
-    <div>我的网站,欧耶\(^o^)/</div>
-    <div @click="httpTest()">66666</div>
-    <div v-if="mList.length > 0" v-for="item in mList" :key="item.id">
-    <span>Id: {{ item.id }} == </span>
-    <span>Name: {{ item.name }}</span>
+  <div class="col-sm-9 col-sm-offset-1  main">
+    <h2 class="sub-header">Section title</h2>
+    <a class="btn btn-success" href="/students/new">添加学生</a>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>姓名</th>
+          <th>性别</th>
+          <th>年龄</th>
+          <th>爱好</th>
+          <th>操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item, index) in mList" :key="index">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.sex }}</td>
+          <td>{{ item.age }}</td>
+          <td>{{ item.hobbies }}</td>
+          <td>
+            <a href="/students/edit?id=4">编辑</a>
+            <a href="/students/delete?id=4">删除</a>
+          </td>
+        </tr>
+        </tbody>
+      </table>
     </div>
-    <router-view/>
+
   </div>
 </template>
 
@@ -17,27 +40,31 @@
         mList:[]
       }
     },
+    created(){
+      this.getData();
+    },
     methods: {
-      httpTest(){
-        // var _self = this;
+      getData(){
+        var _self = this;
         // this.$ajax.get("http://39.107.111.195:8888/", {
-        //   params: {
-        //     'userId': 'yulin',
-        //     'date': new Date()
-        //   }
-        // })
-        //   .then(function (res) {
-        //     _self.mList = res.data;
-        //     console.log("成功=="+JSON.stringify(res));
-        //   })
-        //   .catch(function (err) {
-        //     console.log("失败=="+JSON.stringify(err))
-        //   });
-
-        this.$router.push({
-          path: '/first',
-          name: 'first'
+          this.$ajax.get("http://localhost:8888/", {
+          params: {
+            'userId': 'yulin',
+            'date': new Date()
+          }
         })
+          .then(function (res) {
+            _self.mList = res.data;
+            console.log("成功=="+JSON.stringify(res));
+          })
+          .catch(function (err) {
+            console.log("失败=="+JSON.stringify(err))
+          });
+
+        // this.$router.push({
+        //   path: '/first',
+        //   name: 'first'
+        // })
       }
 
     }
@@ -45,12 +72,24 @@
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  .sub-header {
+    padding-bottom: 10px;
+    border-bottom: 1px solid #eee;
   }
+  .main {
+    padding: 20px;
+  }
+
+  @media (min-width: 768px) {
+    .main {
+      padding-right: 40px;
+      padding-left: 40px;
+    }
+  }
+
+  .sub-header {
+    padding-bottom: 10px;
+    border-bottom: 1px solid #eee;
+  }
+
 </style>
