40fb052ee0305e4a271881011d45a37b07f4e947
yulin <2441518624@qq.com>
2019-06-27 15:04:15 +0800

    样式修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,38 @@
 <template>
-  <div class="col-sm-9 col-sm-offset-1  main">
-    <h2 class="sub-header">Section title</h2>
-    <a class="btn btn-success" href="/students/new">添加学生</a>
-    <div class="table-responsive">
-      <table class="table table-striped">
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>姓名</th>
-          <th>性别</th>
-          <th>年龄</th>
-          <th>爱好</th>
-          <th>操作</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="(item, index) in mList" :key="index">
-          <td>{{ item.id }}</td>
-          <td>{{ item.name }}</td>
-          <td>{{ item.sex }}</td>
-          <td>{{ item.age }}</td>
-          <td>{{ item.hobbies }}</td>
-          <td>
-            <a href="/students/edit?id=4">编辑</a>
-            <a href="/students/delete?id=4">删除</a>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+  <div class="container-fluid">
+    <div class="row main">
+      <h2 class="sub-header">Section title</h2>
+      <div class="text-right">
+        <a class="btn btn-success" href="/students/new">添加学生</a>
+      </div>
+      <div class="table-responsive">
+        <table class="table table-striped table-hover text-left">
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>姓名</th>
+            <th>性别</th>
+            <th>年龄</th>
+            <th>爱好</th>
+            <th>操作</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(item, index) in mList" :key="index">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.sex }}</td>
+            <td>{{ item.age }}</td>
+            <td>{{ item.hobbies }}</td>
+            <td>
+              <a href="/students/edit?id=4">编辑</a>
+              <a href="/students/delete?id=4">删除</a>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
     </div>
 
   </div>
@@ -47,11 +52,13 @@
       getData(){
         var _self = this;
         // this.$ajax.get("http://39.107.111.195:8888/", {
-          this.$ajax.get("http://localhost:8888/", {
-          params: {
-            'userId': 'yulin',
-            'date': new Date()
-          }
+        //   this.$ajax.get("http://localhost:8888/", {
+        //   this.$ajax.get("http://localhost:3000/", {
+          this.$ajax.get("http://localhost:3000/userList/getUserList", {
+          // params: {
+          //   'userId': 'yulin',
+          //   'date': new Date()
+          // }
         })
           .then(function (res) {
             _self.mList = res.data;
@@ -61,10 +68,7 @@
             console.log("失败=="+JSON.stringify(err))
           });
 
-        // this.$router.push({
-        //   path: '/first',
-        //   name: 'first'
-        // })
+
       }
 
     }
@@ -77,7 +81,8 @@
     border-bottom: 1px solid #eee;
   }
   .main {
-    padding: 20px;
+    padding-top: 10px;
+    margin: 0 100px;
   }
 
   @media (min-width: 768px) {
